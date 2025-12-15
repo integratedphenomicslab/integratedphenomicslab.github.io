07089458af77b42e7706e49e35b574d48817fabf
yuchenchang <tommy820531@gmail.com>
2025-12-15 08:40:47 +0000

    Deploying to gh-pages from @ hsiehslab/hsiehslab.github.io@6e6606ad3032ad845bc6cf4aa4308fbd9d651057 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-news",
-          title: "news",
+          title: "NEWS",
           description: "",
           section: "Navigation",
           handler: () => {
